e28db9c4f32a9c3a3c19bc5434c8b09044fd8c25
John Areola <73708569+Jonnie-Dev@users.noreply.github.com>
2024-01-09 07:54:29 +0000

    final commit

--- member.js
+++ member.js
@@ -1,5 +1,5 @@
 function skillsMember() {
-  var skills = document.getElementById("skills").value;
+  var skills = document.getElementById("skill").value;
   var skillsError = document.getElementById("skillsError");
   if (skills == "") {
     skillsError.innerHTML = "Please enter your skills";
